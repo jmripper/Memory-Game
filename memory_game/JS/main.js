43b02c2e63f4a5fad3e865839798701bf0924ed3
Jenny_Ripper <jennifer.ripper@gmail.com>
2019-11-10 16:29:10 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,6 +1,24 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var catdFour = "king";
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push('cardOne');
+cardsInPlay;
+
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+cardsInPlay;
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+} 
